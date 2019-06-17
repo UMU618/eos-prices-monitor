aad1af17fb6ee4cf2a776ea98f49730c37977540
UMU618 <umu618@hotmail.com>
2019-06-17 20:36:43 +0800

    * BTC

--- eos-prices-monitor.py
+++ eos-prices-monitor.py
@@ -30,18 +30,33 @@ def send_dingtalk_msg(content, robot_id):
 if __name__ == '__main__':
     setUrl('https://api.huobi.pro', 'https://api.huobi.pro')
     api = HBRestAPI()
-    price = api.get_last_ticker(symbol='eosusdt')['tick']['data'][0]['price']
-    print('Now price:', price)
+    btc_price = api.get_last_ticker(symbol='btcusdt')['tick']['data'][0]['price']
+    print('Now BTC price:', btc_price)
+    eos_price = api.get_last_ticker(symbol='eosusdt')['tick']['data'][0]['price']
+    print('Now EOS price:', eos_price)
     while True:
         time.sleep(conf.interval)
+
         try:
-            newprice = api.get_last_ticker(symbol='eosusdt')['tick']['data'][0]['price']
-            if newprice > price:
-                if (newprice - price) / price > conf.diff_precent:
-                    send_dingtalk_msg('Price increase to ' + newprice, conf.robot_id)
+            new_btc_price = api.get_last_ticker(symbol='btcusdt')['tick']['data'][0]['price']
+            if new_btc_price > btc_price:
+                if (new_btc_price - btc_price) / btc_price > conf.diff_precent:
+                    send_dingtalk_msg('Price increase to ' + str(new_btc_price), conf.robot_id)
             else:
-                if (price - newprice) / price > conf.diff_precent:
-                    send_dingtalk_msg('Price fall to ' + conf.diff_precent, conf.robot_id)
-            price = newprice
+                if (btc_price - new_btc_price) / btc_price > conf.diff_precent:
+                    send_dingtalk_msg('Price fall to ' + str(conf.diff_precent), conf.robot_id)
+            btc_price = new_btc_price
         except Exception as err:
-            print('fail to get last price, exception:', err)
+            print('fail to get last BTC price, exception:', err)
+
+        try:
+            new_eos_price = api.get_last_ticker(symbol='eosusdt')['tick']['data'][0]['price']
+            if new_eos_price > eos_price:
+                if (new_eos_price - eos_price) / eos_price > conf.diff_precent:
+                    send_dingtalk_msg('Price increase to ' + str(new_eos_price), conf.robot_id)
+            else:
+                if (eos_price - new_eos_price) / eos_price > conf.diff_precent:
+                    send_dingtalk_msg('Price fall to ' + str(conf.diff_precent), conf.robot_id)
+            eos_price = new_eos_price
+        except Exception as err:
+            print('fail to get last EOS price, exception:', err)
