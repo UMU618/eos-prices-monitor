7ae0ff22d21ba7311a27720ee3c7cbadc0b55b55
UMU618 <umu618@hotmail.com>
2019-06-17 21:27:07 +0800

    *

--- conf.py
+++ conf.py
@@ -1,3 +1,4 @@
 robot_id = 'UMUTech@WeChat'
 interval = 15 * 60
-diff_precent = 0.04
+diff_precent = 0.005
+last_24h_precent = 0.03
--- eos-prices-monitor.py
+++ eos-prices-monitor.py
@@ -30,20 +30,77 @@ def send_dingtalk_msg(content, robot_id):
 if __name__ == '__main__':
     setUrl('https://api.huobi.pro', 'https://api.huobi.pro')
     api = HBRestAPI()
+
+    btc_last = 0
+    eos_last = 0
+
     btc_price = api.get_last_ticker(symbol='btcusdt')['tick']['data'][0]['price']
     print('Now BTC price:', btc_price)
     eos_price = api.get_last_ticker(symbol='eosusdt')['tick']['data'][0]['price']
     print('Now EOS price:', eos_price)
+
     while True:
+        try:
+            btc_kline = api.get_last_24h_kline(symbol='btcusdt')
+            if btc_kline['status'] == 'ok':
+                tick = btc_kline['tick']
+                if tick['open'] < tick['close']:
+                    if (tick['close'] - tick['open']) / tick['open'] >= conf.last_24h_precent:
+                        if btc_last == 0:
+                            btc_last = tick['close']
+                            send_dingtalk_msg('BTC price increase to ' + str(tick['close']), conf.robot_id)
+                        else:
+                            if btc_last < tick['close']:
+                                btc_last = tick['close']
+                                send_dingtalk_msg('BTC price increase to ' + str(tick['close']), conf.robot_id)
+                else:
+                    if (tick['open'] - tick['close']) / tick['open'] >= conf.last_24h_precent:
+                        if btc_last == 0:
+                            btc_last = tick['close']
+                            send_dingtalk_msg('BTC price fall to ' + str(tick['close']), conf.robot_id)
+                        else:
+                            if btc_last > tick['close']:
+                                btc_last = tick['close']
+                                send_dingtalk_msg('BTC price fall to ' + str(tick['close']), conf.robot_id)
+
+        except Exception as err:
+            print('fail to get last 24h BTC kline, exception:', err)
+
+        try:
+            eos_kline = api.get_last_24h_kline(symbol='eosusdt')
+            if eos_kline['status'] == 'ok':
+                tick = eos_kline['tick']
+                if tick['open'] < tick['close']:
+                    if (tick['close'] - tick['open']) / tick['open'] >= conf.last_24h_precent:
+                        if eos_last == 0:
+                            eos_last = tick['close']
+                            send_dingtalk_msg('EOS price increase to ' + str(tick['close']), conf.robot_id)
+                        else:
+                            if eos_last < tick['close']:
+                                eos_last = tick['close']
+                                send_dingtalk_msg('EOS price increase to ' + str(tick['close']), conf.robot_id)
+                else:
+                    if (tick['open'] - tick['close']) / tick['open'] >= conf.last_24h_precent:
+                        if eos_last == 0:
+                            eos_last = tick['close']
+                            send_dingtalk_msg('EOS price fall to ' + str(tick['close']), conf.robot_id)
+                        else:
+                            if eos_last > tick['close']:
+                                eos_last = tick['close']
+                                send_dingtalk_msg('EOS price fall to ' + str(tick['close']), conf.robot_id)
+
+        except Exception as err:
+            print('fail to get last 24h EOS kline, exception:', err)
+
         time.sleep(conf.interval)
 
         try:
             new_btc_price = api.get_last_ticker(symbol='btcusdt')['tick']['data'][0]['price']
             if new_btc_price > btc_price:
-                if (new_btc_price - btc_price) / btc_price > conf.diff_precent:
+                if (new_btc_price - btc_price) / btc_price >= conf.diff_precent:
                     send_dingtalk_msg('Price increase to ' + str(new_btc_price), conf.robot_id)
             else:
-                if (btc_price - new_btc_price) / btc_price > conf.diff_precent:
+                if (btc_price - new_btc_price) / btc_price >= conf.diff_precent:
                     send_dingtalk_msg('Price fall to ' + str(conf.diff_precent), conf.robot_id)
             btc_price = new_btc_price
         except Exception as err:
@@ -52,10 +109,10 @@ if __name__ == '__main__':
         try:
             new_eos_price = api.get_last_ticker(symbol='eosusdt')['tick']['data'][0]['price']
             if new_eos_price > eos_price:
-                if (new_eos_price - eos_price) / eos_price > conf.diff_precent:
+                if (new_eos_price - eos_price) / eos_price >= conf.diff_precent:
                     send_dingtalk_msg('Price increase to ' + str(new_eos_price), conf.robot_id)
             else:
-                if (eos_price - new_eos_price) / eos_price > conf.diff_precent:
+                if (eos_price - new_eos_price) / eos_price >= conf.diff_precent:
                     send_dingtalk_msg('Price fall to ' + str(conf.diff_precent), conf.robot_id)
             eos_price = new_eos_price
         except Exception as err:
